df690854cfd344a2566baea3d10ac56fcb5c2695
gilgenbergg <lanasvega@gmail.com>
2019-01-09 14:09:09 +0300

    finished

--- Multi-dimensional array/Programming (8)/src/Main.java
+++ Multi-dimensional array/Programming (8)/src/Main.java
@@ -1,6 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    int dim = sc.nextInt();
+    int[][] pre = new int[dim][dim];
+    int current = 1;
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        pre[i][j] = current;
+        current++;
+      }
+    }
+    int line_step = 1;
+    int t = 0;
+    int b = dim - 1;
+    int l = 0;
+    int r = dim - 1;
+    int dir = 0;
+    int[][] res = new int[dim][dim];
+    int cell = 1;
+    while (cell <= dim*dim) {
+      if (dir==0) {
+        for (int j = l; j <= r; j++) {
+          res[t][j] = cell;
+          cell++;
+        }
+        t++;
+      }
+      else if (dir==1)
+      {
+        for (int i = t; i<=b; i++) {
+          res[i][r] = cell;
+          cell++;
+        }
+        r--;
+      }
+      else if (dir ==2){
+        for (int j=r; j>=l; j--) {
+          res[b][j] = cell;
+          cell++;
+        }
+        b--;
+      }
+      else if (dir==3) {
+        for (int i=b; i>=t; i--) {
+          res[i][l] = cell;
+          cell++;
+        }
+        l++;
+      }
+      dir = (dir+1)%4;
+    }
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        System.out.print(res[i][j]+" ");
+      }
+      System.out.println();
+    }
   }
 }
--- Multi-dimensional array/Programming (9)/src/Main.java
+++ Multi-dimensional array/Programming (9)/src/Main.java
@@ -1,6 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    //put your code here
+    Scanner sc = new Scanner(System.in);
+    List<Integer[]> pre = new ArrayList<>();
+    String line = sc.nextLine();
+    while(!line.equals("end")) {
+      String[] elements = line.split(" ");
+      Integer[] elems = new Integer[elements.length];
+      for (int i=0; i<elems.length; i++) {
+        elems[i] = Integer.valueOf(elements[i]);
+      }
+      pre.add(elems);
+      line = sc.nextLine();
+    }
+    int rows = pre.size();
+    int columns = pre.get(0).length;
+    int res[][] = new int[rows][columns];
+    int t, b, l, r, cell;
+    for (int i=0; i<rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        if (i-1>=0)
+          t = pre.get(i-1)[j];
+        else
+          t = pre.get(rows - 1)[j];
+        if (i+1<=rows - 1)
+          b = pre.get(i+1)[j];
+        else
+          b = pre.get(0)[j];
+        if (j-1>=0)
+          l = pre.get(i)[j - 1];
+        else
+          l = pre.get(i)[columns - 1];
+        if (j+1<=columns - 1)
+          r = pre.get(i)[j+1];
+        else
+          r = pre.get(i)[0];
+        cell = t+b+l+r;
+        res[i][j] = cell;
+      }
+    }
+    for (int i=0; i<rows;i++) {
+      for (int j=0; j<columns; j++) {
+        System.out.print(res[i][j]+" ");
+      }
+      System.out.println();
+    }
   }
-}
+  }
